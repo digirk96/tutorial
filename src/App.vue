58e2e7375c860cb1141a77f64045533b025552b1
Rakesh Kumar Singh <digirk96@gmail.com>
2022-10-25 09:45:30 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header title="Remember Me"></the-header>
+<the-resource></the-resource>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import TheHeader from './components/layouts/TheHeader.vue'
+
+import TheResource from './components/learningresource/TheResource.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+      TheResource,
+        TheHeader
+    },
+
+ 
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap');
+*{
+  box-sizing: border-box;
 }
+html{
+  font-family: 'Roboto', sans-serif;
+}
+body{
+  margin: 0;
+}
+
 </style>
